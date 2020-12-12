095b41e3c585463697e2135c637bd8ccaadf60b6
masai1998 <202028061022@mail.bnu.edu.cn>
2020-12-12 19:39:39 +0800

    2020.12.12

--- roi/integrate_roi.py
+++ roi/integrate_roi.py
@@ -8,36 +8,21 @@ import os
 import nibabel as nib
 import numpy as np
 
-class integration(object):
+Head_roi = '/nfs/e4/function_guided_resection/MotorMapping/derivatives/surface/roi/Head'
+a_path = os.path.join(Head_roi, 'L_Head_z=4.7_p=0.25_region-a.nii.gz')
+b_path = os.path.join(Head_roi, 'L_Head_z=4.7_p=0.25_region-b.nii.gz')
+c_path = os.path.join(Head_roi, 'L_Head_z=4.7_p=0.25_region-c.nii.gz')
+d_path = os.path.join(Head_roi, 'L_Head_z=4.7_p=0.25_region-d.nii.gz')
 
-    def __init__(self, roi_dir, header, savename):
-        self.roi_dir = roi_dir
-        self.header = header
-        self.savename = savename
+a = nib.load(a_path).get_fdata()
+b = nib.load(b_path).get_fdata()
+c = nib.load(c_path).get_fdata()
+d = nib.load(d_path).get_fdata()
 
-    def integrate_roi(self):
+header = nib.load(a_path).head
 
-        # read and integrate data
+data = a + b+ c + d
 
-        data = None
-        roi_files_list = os.listdir(self.roi_dir)
-        for roi_file in roi_files_list:
-            roi_file_path = os.path.join(self.roi_dir, roi_file)
-            temp_data = nib.load(roi_file_path).get_fdata
-            data = data + temp_data
+image = nib.Nifti1Image(data, header)
+nib.save(image, Head_roi + 'L_Head_z=4.7_p=0.25.nii.gz')
 
-        # save roi file
-
-        integrated_file_path = os.path.join(self.roi_dir, self.savename, '.nii.gz')
-        image = nib.Nifti2Image(data, self.header)
-        nib.nifti2.save(image, integrated_file_path)
-
-
-
-if __name__ == '__main__':
-    roi_dir = '/nfs/e4/function_guided_resection/MotorMapping/derivatives/surface/roi/Head'
-    header = nib.load('/nfs/e4/function_guided_resection/MotorMapping/derivatives/surface/roi/Head/L_Head_z=4.7_p=0.25_region-a.nii.gz').header
-    savename = 'L_Head_z=4.7_p=0.25'
-
-    integration(roi_dir, header, savename)
-    integration.integrate_roi()
