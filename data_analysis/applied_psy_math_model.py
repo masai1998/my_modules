360b23f5fcfc8f67541136101c8a059325c6cdb1
masai1998 <202028061022@mail.bnu.edu.cn>
2020-12-17 23:04:25 +0800

    Update applied_psy_math_model.py

--- data_analysis/applied_psy_math_model.py
+++ data_analysis/applied_psy_math_model.py
@@ -10,20 +10,13 @@ import matplotlib.pyplot as plt
 import statsmodels.api as sm
 
 def data_analysis(data_file):
-
     # read data
-
     data = pd.read_excel(data_file, usecols=[0, 2, 3, 4])
     data.columns = ['group_id', 'y', 'x1', 'x2']
-    # print(data)
-
     # prepare x and y
-
     x = sm.add_constant(data.iloc[:, 2:])
     y = data['y']
-
     # compute model
-
     model = sm.OLS(y, x)
     result = model.fit()
     print(result.summary())
