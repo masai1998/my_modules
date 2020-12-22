150283406517544549cbd4f99c9dadcfcbe992bb
masai1998 <202028061022@mail.bnu.edu.cn>
2020-12-22 23:39:50 +0800

    Create max_pmap_script.py

---
+++ roi/max_pmap_script.py
@@ -0,0 +1,36 @@
+"""
+    max_pmap.py
+"""
+
+
+
+import os
+import numpy as np
+import pandas as pd
+import nibabel as nib
+
+Head_file = '/nfs/e4/function_guided_resection/MotorMapping/derivatives/surface/probabilistic_map/Head.dscalar.nii'
+Upperlimbs_file = '/nfs/e4/function_guided_resection/MotorMapping/derivatives/surface/probabilistic_map/UpperLimbs.dscalar.nii'
+Lowerlimbs_file = '/nfs/e4/function_guided_resection/MotorMapping/derivatives/surface/probabilistic_map/LowerLimbs.dscalar.nii'
+
+Head_data = nib.load(Head_file).get_fdata()
+Upperlimbs_data = nib.load(Upperlimbs_file).get_fdata()
+Lowerlimbs_data = nib.load(Lowerlimbs_file).get_fdata()
+
+# 1, 91282
+
+first_column = np.zeros(91282).reshape((-1,1))
+Head_data = np.where(Head_data > 0.25, Head_data, 0).reshape((-1,1))
+Upperlimbs_data = np.where(Upperlimbs_data > 0.25, Upperlimbs_data, 0).reshape((-1,1))
+Lowerlimbs_data = np.where(Lowerlimbs_data > 0.25, Lowerlimbs_data, 0).reshape((-1,1))
+pmap_matrix = np.c_[first_column, Head_data, Upperlimbs_data, Lowerlimbs_data]
+
+max_pmap_data = np.argmax(pmap_matrix, axis = 1)
+
+
+
+
+
+np.set_printoptions(threshold=np.inf)
+print(max_pmap_data)
+print(max_pmap_data.shape)
