844a2b226e4617b2d0fb9b1f83cf905dd85208fa
masai1998 <202028061022@mail.bnu.edu.cn>
2020-12-26 09:09:14 +0800

    Update sftp.py

--- sftp&rename/sftp.py
+++ sftp&rename/sftp.py
@@ -12,12 +12,12 @@ if __name__ == '__main__':
     port = 22
     username = 'masai'
     password = 'ms4020665'
-    subjectlist = []
-    temp_list = os.listdir(r'D:\MotorMapping_fixed_except_sub-retestM02')
 
-    for file in temp_list:
-        if 'sub-M' in file:
-            subjectlist.append(file)
+    subjectlist = ['sub-M01', 'sub-M02', 'sub-M03', 'sub-M04', 'sub-M05', 'sub-M06', 'sub-M07', 'sub-M08', 'sub-M09', 'sub-M10']
+#    temp_list = os.listdir(r'D:\MotorMapping_fixed_except_sub-retestM02')
+#    for file in temp_list:
+#        if 'sub-M' in file:
+#           subjectlist.append(file)
     #print(subjectlist)
 
     print('connecting...')
@@ -28,7 +28,7 @@ if __name__ == '__main__':
 
     for subject in subjectlist:
         level2_results_dir = r'/nfs/e4/function_guided_resection/MotorMapping/derivatives/surface/ciftify/' + subject + r'/MNINonLinear/Results/ses-01_task-motor/ses-01_task-motor_hp200_s4_level2.feat/'
-        print(level2_results_dir)
+        #print(level2_results_dir)
         pwd_list = sftp.listdir(level2_results_dir)
         for result_file in pwd_list:
             if 'zstat_Head' in result_file:
@@ -36,7 +36,7 @@ if __name__ == '__main__':
 #                local = os.path.join('D:\level2_results\Head', subject)
 #                if not os.path.exists(local):
 #                    os.makedirs(local)
-                localpath = os.path.join('D:\level2_results\Head', result_file)
+                localpath = os.path.join('/Users/masai/Documents/Head_zstat', result_file)
                 # print(remote)
                 # print(localpath)
                 sftp.get(remote, localpath)
