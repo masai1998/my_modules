4a56ce3d61001267e63bc60801abc7a96916d441
masai1998 <202028061022@mail.bnu.edu.cn>
2021-03-05 17:22:50 +0800

    Update max_pmap_script.py

--- roi/max_pmap_script.py
+++ roi/max_pmap_script.py
@@ -4,9 +4,7 @@
 
 
 
-import os
 import numpy as np
-import pandas as pd
 import nibabel as nib
 
 Head_file = '/nfs/e4/function_guided_resection/MotorMapping/derivatives/surface/probabilistic_map/Head.dscalar.nii'
@@ -19,15 +17,31 @@ Lowerlimbs_data = nib.load(Lowerlimbs_file).get_fdata()
 
 # 1, 91282
 
-first_column = np.zeros(91282).reshape((-1,1))
-Head_data = np.where(Head_data > 0.25, Head_data, 0).reshape((-1,1))
-Upperlimbs_data = np.where(Upperlimbs_data > 0.25, Upperlimbs_data, 0).reshape((-1,1))
-Lowerlimbs_data = np.where(Lowerlimbs_data > 0.25, Lowerlimbs_data, 0).reshape((-1,1))
-pmap_matrix = np.c_[first_column, Head_data, Upperlimbs_data, Lowerlimbs_data]
+# first_column = np.zeros(91282).reshape((-1,1))
+# Head_data = np.where(Head_data > 0.25, Head_data, 0).reshape((-1,1))
+# Upperlimbs_data = np.where(Upperlimbs_data > 0.25, Upperlimbs_data, 0).reshape((-1,1))
+# Lowerlimbs_data = np.where(Lowerlimbs_data > 0.25, Lowerlimbs_data, 0).reshape((-1,1))
+# pmap_matrix = np.c_[first_column, Head_data, Upperlimbs_data, Lowerlimbs_data]
+#
+# max_pmap_data = np.argmax(pmap_matrix, axis = 1).reshape((1,-1))
+#
+# header = nib.load(Head_file).header
+# save_path = 'pmap.dscalar.nii'
+# image = nib.Cifti2Image(max_pmap_data, header)
+# nib.cifti2.save(image, save_path)
 
-max_pmap_data = np.argmax(pmap_matrix, axis = 1).reshape((1,-1))
+Head_data = np.squeeze(Head_data)
+Upperlimbs_data = np.squeeze(Upperlimbs_data)
+Lowerlimbs_data = np.squeeze(Lowerlimbs_data)
+
+first_column = np.zeros(91282)
+Head_data = np.where(Head_data > 0.25, Head_data, 0)
+Upperlimbs_data = np.where(Upperlimbs_data > 0.25, Upperlimbs_data, 0)
+Lowerlimbs_data = np.where(Lowerlimbs_data > 0.25, Lowerlimbs_data, 0)
+pmap_matrix = np.c_[first_column, Head_data, Upperlimbs_data, Lowerlimbs_data]
+max_pmap_data = np.argmax(pmap_matrix, axis = 1)[None, :]
 
 header = nib.load(Head_file).header
-save_path = 'pmap.dscalar.nii'
+save_path = 'max_pmap.dscalar.nii'
 image = nib.Cifti2Image(max_pmap_data, header)
 nib.cifti2.save(image, save_path)
