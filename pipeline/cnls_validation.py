a4555ef81e33967fc94332f4ff1e410b924a533b
masai1998 <202028061022@mail.bnu.edu.cn>
2021-03-11 17:32:30 +0800

    Update cnls_validation.py

--- pipeline/cnls_validation.py
+++ pipeline/cnls_validation.py
@@ -42,6 +42,7 @@ def cnls_validation(args):
     dicom_dir = pjoin(args.projectdir, 'data', 'bold', 'dicom')
     nifti_dir = pjoin(args.projectdir, 'data', 'bold', 'nifti')
     info_dir = pjoin(args.projectdir, 'data', 'bold', 'info')
+    derivatives_dir = pjoin(args.projectdir, 'data', 'bold', 'derivatives')
     if args.create:
         mkdir = 1
     else:
@@ -54,58 +55,13 @@ def cnls_validation(args):
     if not os.path.exists(args.scaninfo):
         logging.critical('scaninfo file in {} is not found!'.format(args.scaninfo))
         raise AssertionError(bcolors.FAIL + "[Critical] NOT FOUND {}".format(args.scaninfo) + bcolors.ENDC)
-    for path in [bold_dir, orig_dir, dicom_dir, nifti_dir]:
+    for path in [bold_dir, orig_dir, dicom_dir, nifti_dir, info_dir, derivatives_dir]:
         check_path(path, mkdir)
 
     # Check if the orig_dir is empty
     if not os.listdir(orig_dir):
         logging.critical('orig_dir in {} is empty!'.format(orig_dir))
         raise AssertionError("[Critical] orig_dir in {} is empty!".format(orig_dir) + bcolors.ENDC)
-
-        # if check_path(bold_dir, mkdir) == 0:
-    #     logging.warning('{} is just created'.format(bold_dir))
-    #     print("[Warning] {} is just created".format(bold_dir))
-    # if check_path(bold_dir, mkdir) == 1:
-    #     logging.warning('{} is not existed'.format(bold_dir))
-    #     print("[Warning] {} is just created".format(bold_dir))
-    # # orig
-    # if check_path(orig_dir, mkdir) == 0:
-    #     logging.warning('{} is just created'.format(orig_dir))
-    #     print("[Warning] {} is just created".format(orig_dir))
-    #
-    # if bold_dir:
-    #     os.chdir(bold_dir)
-    #     # scaninfo
-    #     if not os.path.exists(args.scaninfo):
-    #         logging.critical('NOT FOUND {}'.format(args.scaninfo))
-    #         raise AssertionError(bcolors.FAIL + "[Error] NOT FOUND {}".format(args.scaninfo) + bcolors.ENDC)
-    #
-    #     check_results = [check_path(_) for _ in [args.orig, args.dicom, args.nifti]]
-    #     if check_results[0] == 0:
-    #         logging.warning('{} is just created, it should be prepared before.'.format(args.orig))
-    #         print("[Warning] {} is just created, it should be prepared before.".format(args.orig))
-    #         if not os.listdir(args.orig):
-    #             logging.critical('No files found in {}'.format(args.orig))
-    #             raise AssertionError("No files found in {}".format(args.orig))
-    #     if check_results[1] == 0:
-    #         logging.warning('{} is just created, it should be prepared before.'.format(args.dicom))
-    #         print("[Warning] {} is just created, it should be prepared before.".format(args.dicom))
-    #     if check_results[2] == 0:
-    #         logging.warning('{} is just created, it should be prepared before.'.format(args.nifti))
-    #         print("[Warning] {} is just created, it should be prepared before.".format(args.nifti))
-    #     if check_path(pjoin(args.orig, 'code')) == 0:
-    #         logging.warning('{} is just created, it should be prepared before.'.format(pjoin(args.orig, 'code')))
-    #         print("[Warning] {} is just created, it should be prepared before.".format(pjoin(args.orig, 'code')))
-    #
-    # else:
-    #     if not os.path.exists(args.scaninfo):
-    #         logging.critical("NOT FOUND {}".format(args.scaninfo))
-    #         raise AssertionError(bcolors.FAIL + "[Error] NOT FOUND {}".format(args.scaninfo) + bcolors.ENDC)
-    #     expect_folds = np.array([args.orig, args.dicom, args.nifti])
-    #     absent_folds = expect_folds[np.array([check_path(_, 0) for _ in expect_folds])]
-    #     if absent_folds:
-    #         logging.critical("NOT FOUND {}".format(absent_folds))
-    #         raise AssertionError(bcolors.FAIL + "[Error] NOT FOUND {}".format(absent_folds) + bcolors.ENDC)
 
     print('Log is saved in {}.'.format(pjoin(bold_dir, 'info', 'CNLS_validation.log')))
 
